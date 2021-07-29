de2bfec0f29a8f28e98d09fa19b94370f6008c36
ElizabethJanes <elizabeth_j@rogers.com>
2021-07-29 11:24:23 -0400

    Update Snapshot tests

--- src/__tests__/components/drs/formComponents/AwsS3AccessObjects.test.js
+++ src/__tests__/components/drs/formComponents/AwsS3AccessObjects.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import AwsS3AccessObjects from '../../../../lib/components/drs/formComponents/AwsS3AccessObjects';
-import { unmountComponentAtNode } from "react-dom";
 
-let container = null;
 let emptyListOfAwsS3AccessObjects = null;
 let populatedListOfAwsS3AccessObjects = null;
 beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
     emptyListOfAwsS3AccessObjects = [];
     populatedListOfAwsS3AccessObjects = [
         {region: 'region1', bucket: 'bucket1', key: 'key1'}, 
@@ -17,44 +16,26 @@ beforeEach(() => {
     ];
 });
 
-afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-});
-
-test('SHOW <AwsS3AccessObjects /> should handle 0 AWS S3 access objects', () => {
-    const awsS3AccessObjects = renderer
-    .create(<AwsS3AccessObjects 
-        readOnly={true}
-        aws_s3_access_objects={emptyListOfAwsS3AccessObjects}/>)
-    .toJSON();
-    expect(awsS3AccessObjects).toMatchSnapshot();
-});
-
 test('SHOW <AwsS3AccessObjects /> should handle multiple AWS S3 access objects', () => {
-    const awsS3AccessObjects = renderer
-    .create(<AwsS3AccessObjects 
-        readOnly={true}
-        aws_s3_access_objects={populatedListOfAwsS3AccessObjects}/>)
-    .toJSON();
-    expect(awsS3AccessObjects).toMatchSnapshot();
+    let {container} = render(
+        <AwsS3AccessObjects readOnly={true}
+        aws_s3_access_objects={populatedListOfAwsS3AccessObjects}/>
+    );
+    expect(container.firstChild).toMatchSnapshot();
 });
 
 test('NEW and EDIT <AwsS3AccessObjects /> should handle 0 AWS S3 access objects', () => {
-    const awsS3AccessObjects = renderer
-    .create(<AwsS3AccessObjects 
+    let {container} = render(<AwsS3AccessObjects 
         readOnly={false}
-        aws_s3_access_objects={emptyListOfAwsS3AccessObjects}/>)
-    .toJSON();
-    expect(awsS3AccessObjects).toMatchSnapshot();
+        aws_s3_access_objects={emptyListOfAwsS3AccessObjects}/>
+    );
+    expect(container.firstChild).toMatchSnapshot();
 });
 
 test('NEW and EDIT <AwsS3AccessObjects /> should handle multiple AWS S3 access objects', () => {
-    const awsS3AccessObjects = renderer
-    .create(<AwsS3AccessObjects 
+    let {container} = render(<AwsS3AccessObjects 
         readOnly={false}
-        aws_s3_access_objects={populatedListOfAwsS3AccessObjects}/>)
-    .toJSON();
-    expect(awsS3AccessObjects).toMatchSnapshot();
+        aws_s3_access_objects={populatedListOfAwsS3AccessObjects}/>
+    );
+    expect(container.firstChild).toMatchSnapshot();
 });
